f460fd01f9cf52012b12e229a5ecfb9573d43eec
patgo25 <p.krause5994@gmail.com>
2014-08-10 17:20:13 +0200

    Update #002

--- Code/SampleGame.h
+++ Code/SampleGame.h
@@ -6,11 +6,19 @@ private:
 	scene::IAnimatedMesh *map;
 	scene::ISceneManager * smgr;
 	gui::IGUIEnvironment* env;
+	video::IVideoDriver* driver;
 	scene::ICameraSceneNode* camera;
 	scene::IMeshSceneNode *node;
+	scene::IAnimatedMeshSceneNode *dwarf;
 	scene::ITriangleSelector* selector;
 	gui::IGUIWindow *exitWindow;
 	gui::IGUIStaticText* text;
+	scene::ILightSceneNode *light;
+	scene::ISceneNode* highlightedSceneNode, *selectedSceneNode;
+    scene::ISceneCollisionManager* collMan;
+	video::SMaterial material;
+	scene::IBillboardSceneNode *bill;
+	
 	int lastFPS;
 	bool KeyIsDown[KEY_KEY_CODES_COUNT];
 	irr::SKeyMap keyMap[6];
